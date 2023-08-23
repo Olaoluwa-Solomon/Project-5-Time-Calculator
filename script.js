ec15691c132405db2436d6b32186fac009136122
Olaoluwa Solomon <olaolusolomon01@gmail.com>
2023-08-23 10:44:09 +0100

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+ let username = document.getElementsByClassName("username");
+var button = document.getElementById("btn");
+ var Homeinput= document.getElementById("Homeinput");
+  var navigate = document.getElementById("navigate");
+button.addEventListener("click", function (){
+
+ 
+   
+    if(Homeinput.value=== ""){
+        alert("Name cannot be blank");
+        return false;
+} else if(!(isNaN(Homeinput.value))){
+    alert("Please enter text");
+    return false;
+    
+} else {
+navigate.href = "index.html";
+console.log(Homeinput.value);
+return Homeinput.value;
+}
+
+})
+
